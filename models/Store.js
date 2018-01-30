de0fa3ef8a8715b595d89356b3187342afd77310
Glenn R. Fisher <glennrfisher@gmail.com>
2018-01-30 13:05:30 -0600

    Add Store model

---
+++ models/Store.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Please enter a store name!'
+    },
+    slug: String,
+    description: {
+        type: String,
+        trim: true
+    },
+    tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+    if (!this.isModified('name')) return next();
+    this.slug = slug(this.name); // todo: make resilient to duplicate names
+    next();
+});
+
+module.exports = mongoose.model('Store', storeSchema);
