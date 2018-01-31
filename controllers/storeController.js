560dcafca426260f0ae21fa801da53af0d54f5cb
Glenn R. Fisher <glennrfisher@gmail.com>
2018-01-30 21:44:56 -0600

    Add support for updating stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,16 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({ _id: req.params.id });
+    res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+    const query = { _id: req.params.id };
+    const options = { new: true, runValidators: true };
+    const store = await Store.findOneAndUpdate(query, req.body, options).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+    res.redirect(`/stores/${store.id}/edit`);
+}
