a52985789cfe7718e134a3fbf5ed4f160ecd399c
Glenn R. Fisher <glennrfisher@gmail.com>
2018-02-01 19:01:18 -0600

    Only add a store when logged in

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,9 @@ exports.logout = (req, res) => {
     req.flash('success', 'You are now logged out.');
     res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    req.flash('error', 'You must be logged in!');
+    res.redirect('/login');
+};
